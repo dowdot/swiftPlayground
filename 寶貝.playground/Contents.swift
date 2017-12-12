83e59203b7dd6f8530a3b5613d2fd58976e11eb3
dowdot <dowdot@nchu.edu.tw>
2017-12-12 22:11:32 +0800

    寶貝
    定義 function，印出讓自己一秒落淚的溫馨歌詞

---
+++ 寶貝.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func 我的寶貝(varname:String,like:String){  //同時接收二個變數
+    print ("我的寶貝 寶貝 給你一點甜甜")
+    print ("讓你今夜都好眠")
+    print("我的小鬼 小鬼 逗逗你的"+varname)
+    print("讓你喜歡"+like+"\n")
+}
+
+
+func 嘩啦啦啦啦啦(lyrics:String){
+    print ("嘩啦啦啦啦啦 我的寶貝")
+    print (lyrics)
+    print ("唉呀呀呀呀呀 我的寶貝")
+    print ("要你知道你最美\n")
+}
+
+print("張懸\n")
+print("寶貝(in the night)\n")
+print("作詞：張懸  作曲：張懸\n")
+
+我的寶貝(varname:"眉眼",like:"這世界")
+嘩啦啦啦啦啦(lyrics:"倦的時候有個人陪")
+我的寶貝(varname:"小臉",like:"整個明天")
+嘩啦啦啦啦啦(lyrics:"倦的時候有個人陪")
+嘩啦啦啦啦啦(lyrics:"孤單時有人把你想念")
+嘩啦啦啦啦啦(lyrics:"倦的時候有個人陪")
+print("要你知道你最美")
+
